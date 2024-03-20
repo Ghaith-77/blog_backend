074d39b721bbdc7023a143e90fab926e28aabe32
Ghaith <gaesx1x2@gmail.com>
2024-03-20 01:35:53 -0700

    f

--- mudels/postModel.js
+++ mudels/postModel.js
@@ -4,7 +4,7 @@ const joi = require("joi");
 const PostSchema = mongoose.Schema(
   {
     user: {
-      type: mongoose.Types.ObjectId(),
+      type: mongoose.Schema.Types.ObjectId,
       ref: "user",
       required: true,
     },
@@ -23,7 +23,7 @@ const PostSchema = mongoose.Schema(
     },
     likes: [
       {
-       type: mongoose.Types.ObjectId(),
+      type: mongoose.Schema.Types.ObjectId,
         ref: "user",
       }
     ],
@@ -50,13 +50,16 @@ function validationcreatepost(obj) {
   const schema = joi.object({
     title: joi.string().trim().required(),
     description: joi.string().trim().required(),
+    caticory: joi.string().trim().required(),
   });
   return schema.validate(obj);
 }
 function validationPutpost(obj) {
   const schema = joi.object({
-    title: joi.string().trim().required(),
-    description: joi.string().trim().required(),
+    title: joi.string().trim(),
+    description: joi.string().trim(),
+    caticory: joi.string().trim(),
+
   });
   return schema.validate(obj);
 }
